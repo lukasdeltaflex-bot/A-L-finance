1ea440ddee01520c09a72db8977feb7421b6e24c
Lukas Ramos <lukasdeltaflex@gmail.com>
2026-05-11 19:32:55 -0300

    perf: optimize vercel build speed and compatibility

--- vercel.sh
+++ vercel.sh
@@ -2,11 +2,11 @@
 
 # Script para build do Flutter no Vercel
 echo "Instalando o Flutter..."
-git clone https://github.com/flutter/flutter.git -b stable
+git clone https://github.com/flutter/flutter.git -b stable --depth 1
 export PATH="$PATH:`pwd`/flutter/bin"
 
 echo "Rodando pub get..."
 flutter pub get
 
 echo "Fazendo o build para web..."
-flutter build web --release
+flutter build web --release --web-renderer html
