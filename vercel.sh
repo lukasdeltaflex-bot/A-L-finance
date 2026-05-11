a5a16446f05c2bcfdb903e18766ec211b320bf08
Lukas Ramos <lukasdeltaflex@gmail.com>
2026-05-11 19:38:42 -0300

    fix: correct PATH order for flutter in vercel

--- vercel.sh
+++ vercel.sh
@@ -3,10 +3,10 @@
 # Script para build do Flutter no Vercel
 echo "Instalando o Flutter..."
 git clone https://github.com/flutter/flutter.git -b stable --depth 1
-export PATH="$PATH:`pwd`/flutter/bin"
+export PATH="`pwd`/flutter/bin:$PATH"
 
 echo "Rodando pub get..."
 flutter pub get
 
 echo "Fazendo o build para web..."
-flutter build web --release --web-renderer html
+flutter build web --release
