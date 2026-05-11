57e851882cfc53ff2e0ce652073742a8442006fd
Lukas Ramos <lukasdeltaflex@gmail.com>
2026-05-11 20:30:05 -0300

    fix: disable optimization (-O0) to resolve runtime errors

--- vercel.sh
+++ vercel.sh
@@ -12,5 +12,5 @@ flutter doctor
 echo "Rodando pub get..."
 flutter pub get
 
-echo "Fazendo o build para web com otimização reduzida para evitar erros de minificação..."
-flutter build web --release -O 1
+echo "Fazendo o build para web sem otimização (-O0) para garantir que nada quebre..."
+flutter build web --release -O 0
